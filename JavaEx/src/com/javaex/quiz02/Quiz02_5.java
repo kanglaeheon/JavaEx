d400a18bf96fb2e3b1761dbae1e26041f7bd342c
kanglaeheon <kanglaeheon@gmail.com>
2021-07-19 15:09:08 +0900

    Quiz02_5 풀이

---
+++ JavaEx/src/com/javaex/quiz02/Quiz02_5.java
@@ -0,0 +1,17 @@
+package com.javaex.quiz02;
+
+public class Quiz02_5 {
+
+	public static void main(String[] args) {
+		int num1 = 13579, num2 = 13579;
+//		다음 두 값이 같은지 확인하는 코드를 작성해 봅시다.
+//		같다면 "같음" 다르다면 "다름"을 출력하세요.
+//			++num1 + 1
+//			num2++ + 1
+		System.out.println(++num1 + 1 == num2++ + 1 ? "같음" : "다름"); 
+				
+//		최종 num1과 num2의 값을 출력해 봅시다.
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+}
