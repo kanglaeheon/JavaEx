6549680937fab34db71c61070cb000912b45269b
kanglaeheon <kanglaeheon@gmail.com>
2021-07-21 00:14:44 +0900

    제어문 연습 2회차, 2번 3번 연습문제 풀이

--- JavaEx/src/com/javaex/practice01_1/Problem02.java
+++ JavaEx/src/com/javaex/practice01_1/Problem02.java
@@ -1,10 +1,23 @@
 package com.javaex.practice01_1;
 
+import java.util.Scanner;
+
 public class Problem02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
 
+		int i, j, k;// i = 입력 숫자 (증감 x), j = 행 반복 횟수, k = 열 횟수
+		i = scanner.nextInt();
+
+		for (j = 1; j <= i; j++) {
+			for (k = 1; k <= j; k++) {
+				System.out.print(j);
+				}
+			System.out.println("");
+		}
+		scanner.close();
 	}
 
 }
--- JavaEx/src/com/javaex/practice01_1/Problem03.java
+++ JavaEx/src/com/javaex/practice01_1/Problem03.java
@@ -3,7 +3,12 @@ package com.javaex.practice01_1;
 public class Problem03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i,j; // i = 단, j= 곱
+		for ( i = 1; i <= 9; i++) {
+			for ( j = 2; j <= 9; j++) {
+				System.out.print( j + "*" + i + "=" + (j*i) + "\t");
+			} System.out.println(" ");
+		}
 
 	}
 
