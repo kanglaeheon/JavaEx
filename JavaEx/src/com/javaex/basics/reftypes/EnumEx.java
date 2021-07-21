35917cbfeade6f49686d9e7357297dec15d6eef0
kanglaeheon <kanglaeheon@gmail.com>
2021-07-21 13:07:45 +0900

    열거 상수 연습

--- JavaEx/src/com/javaex/basics/reftypes/EnumEx.java
+++ JavaEx/src/com/javaex/basics/reftypes/EnumEx.java
@@ -1,5 +1,7 @@
 package com.javaex.basics.reftypes;
 
+import java.util.Arrays;
+
 public class EnumEx {
 
 	public static void main(String[] args) {
@@ -9,13 +11,19 @@ public class EnumEx {
 	public static void enumEx1() {
 		Week today = Week.WEDNESDAY;
 		
-		System.out.printf("오늘은 %s(%d) 입니다.",
+		System.out.printf("오늘은 %s(%d) 입니다.%n",
 				today.name(),	//	열거 상수 -> 문자열
 				today.ordinal());	//	열거상수 -> 순번
 		
 		Week obj = Week.valueOf("FRIDAY");
 		
-		System.out.printf("%s, %d",
+		System.out.printf("%s, %d%n",
 				obj.name(), obj.ordinal());
+		
+		System.out.printf("%s와 %s의 순번차: %d%n", 
+				today.name(), obj.name(), today.compareTo(obj));
+		
+		Week[] days = Week.values();
+		System.out.println(Arrays.toString(days));
 	}
 }
