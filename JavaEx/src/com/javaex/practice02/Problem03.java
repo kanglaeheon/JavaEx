98fd8f41e652bb359d16cb0a4b0be553e0ca2db7
kanglaeheon <kanglaeheon@gmail.com>
2021-07-21 21:58:22 +0900

    연습 문제 3 푸는 중

---
+++ JavaEx/src/com/javaex/practice02/Problem03.java
@@ -0,0 +1,19 @@
+package com.javaex.practice02;
+
+public class Problem03 {
+
+	public static void main(String[] args) {
+		//	주어진 문자열
+		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l' };
+		
+		int i;	// 카운터
+		
+		for ( i = 0; i < c.length; i++) {
+			int uniCode = (int)c[i];
+		
+		// 푸는 중, 집에 가서 할 것 
+		}
+		
+	}
+
+}
