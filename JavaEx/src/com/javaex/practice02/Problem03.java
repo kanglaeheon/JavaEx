477ddcc5407b79b268eb26e44ea465bd6197f0d1
kanglaeheon <kanglaeheon@gmail.com>
2021-07-22 12:44:27 +0900

    조건문 연습문제 3 풀이

--- JavaEx/src/com/javaex/practice02/Problem03.java
+++ JavaEx/src/com/javaex/practice02/Problem03.java
@@ -6,14 +6,17 @@ public class Problem03 {
 		//	주어진 문자열
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l' };
 		
-		int i;	// 카운터
+		int i,j;	// 카운터
 		
 		for ( i = 0; i < c.length; i++) {
-			int uniCode = (int)c[i];
-		
-		// 푸는 중, 집에 가서 할 것 
-		}
-		
+			System.out.print(c[i]);
+		} System.out.println();
+		for ( i = 0; i < c.length; i++) {
+			switch ( c[i] ) {
+		case ' ' :
+			c[i] = ',';
+			break;
+		} System.out.print(c[i]);
+	} 
 	}
-
 }
