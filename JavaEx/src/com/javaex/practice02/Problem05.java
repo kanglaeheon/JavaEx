f50e26754918805954f6df9af04104aee495b104
kanglaeheon <kanglaeheon@gmail.com>
2021-07-22 21:53:16 +0900

    연습문제 5번 실패, 교재 복습 후 다시 도전

---
+++ JavaEx/src/com/javaex/practice02/Problem05.java
@@ -0,0 +1,40 @@
+//package com.javaex.practice02;
+//
+//public class Problem05 {
+//
+//	public static void main(String[] args) {
+//		int scoreboard[][] = {
+//				{ 80, 75, 90, 95, 78 },
+//				{ 92, 88, 89, 92, 70 },
+//				{ 78, 80, 85, 86, 63 },
+//				{ 83, 84, 89, 87, 75 },
+//				{ 89, 83, 93, 94, 78 }
+//				};
+//		
+//		int i,j;	//	카운터
+//		int sumUpper85 = 0;	//	85이상 점수 합
+//		int avgUpper85 = 0;	//	85이상 점수 평균
+//		int lengthOfScore1 = 0;	//	scoreboard 데이터 범위
+//		int upper85[];	//	85이상 점수 배열
+//		
+//		for ( int score1[] : scoreboard ) {
+//			for (int score2 : score1) {
+//				if ( score2 >= 85 ) { 
+//					upper85[] = 
+//					}
+//				}
+//			}
+//			lengthOfScore1 = score1.length;
+//		}
+//		
+//		avgUpper85 = sumUpper85 / (scoreboard.length * lengthOfScore1) ;
+//		
+//		System.out.println(sumUpper85);
+//		System.out.println(avgUpper85);
+//	}
+//
+//}
+
+
+
+// 교재 본 후 다시 풀 것
