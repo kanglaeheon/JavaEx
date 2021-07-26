05511a389a43ac2727f5e16b8268909a21fa9d31
kanglaeheon <kanglaeheon@gmail.com>
2021-07-26 20:28:50 +0900

    교재 6장 16,17 문제 풀이 / 클래스

---
+++ JavaEx/src/com/javaex/bookquiz/chap06/exercise16/Printer.java
@@ -0,0 +1,19 @@
+package com.javaex.bookquiz.chap06.exercise16;
+
+public class Printer {
+	static	void println(int value1) {
+			System.out.println(value1);
+		}
+		
+	static	public void println(boolean value2) {
+			System.out.println(value2);
+		}
+		
+	static	public void println(double value3) {
+			System.out.println(value3);
+		}
+		
+	static	public void println(String value4) {
+			System.out.println(value4);
+	}
+	}
