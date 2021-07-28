9c20904d6dd775bd06a0136dea85e60bc504bd14
kanglaeheon <kanglaeheon@gmail.com>
2021-07-28 22:59:25 +0900

    교재 8장 확인문제 5번, 익명클래스

--- JavaEx/src/com/javaex/bookquiz/chap08/exercise05/ActionExample.java
+++ JavaEx/src/com/javaex/bookquiz/chap08/exercise05/ActionExample.java
@@ -3,11 +3,11 @@ package com.javaex.bookquiz.chap08.exercise05;
 public class ActionExample {
 
 	public static void main(String[] args) {
-		Action action = new Action() {
+		Action action = new Action() {	//	익명클래스 : 인스턴스 생성과 동시에 오버라이딩
 			public void work() {
-				System.out.println("복사를 합니다");
+				System.out.println("복사를 합니다");	//	오버라이딩
 			}
-		};	//	 ;이 메소드 중괄호 } 뒤에 왜 붙는가?
+		};	//	인스턴스 중괄호 뒤에 ';'
 		action.work();
 	}
 
