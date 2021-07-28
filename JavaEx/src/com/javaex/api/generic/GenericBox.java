d88fc5ca6adc010cbe1002f548d4608d5ce84cec
kanglaeheon <kanglaeheon@gmail.com>
2021-07-28 14:57:53 +0900

    컬렉션 프레임워크 Vector 연습

--- JavaEx/src/com/javaex/api/generic/GenericBox.java
+++ JavaEx/src/com/javaex/api/generic/GenericBox.java
@@ -2,6 +2,14 @@ package com.javaex.api.generic;
 
 //	Generic 이용하기
 //	T : Type의 준말
+
+//	파라미터 갯수는 제한이 없다
+//	주로 사용되는 파라미터 관례:
+//	T : Type
+//	R : Return Type
+//	K : Key
+//	V : Value
+
 public class GenericBox<T> {
 	T Content;
 
