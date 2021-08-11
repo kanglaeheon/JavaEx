04a69d775766eeca4560a256a82c946d8357ed26
kanglaeheon <kanglaeheon@gmail.com>
2021-08-11 14:22:11 +0900

    Multithread Echo Server

--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,13 +1,5 @@
 package com.javaex.network.echoserver;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -27,52 +19,17 @@ public class Server {
 			System.out.println("<서버 시작>");
 			System.out.println("[연결 대기중]");
 			
-			Socket socket = serverSocket.accept();	//	연결 대기
-			
-			//	접속 성공시: 클라이언트 정보 확인
-			InetSocketAddress remote = (InetSocketAddress)socket.getRemoteSocketAddress();
-			System.out.println("[클라이언트 접속 완료]");
-			System.out.println(remote.getAddress() + ":" + remote.getPort());
-			
-			//	클라이언트로부터 메시지 수신
-			//	스트림 열기
-			InputStream is = socket.getInputStream();
-			//	주고 받은 데이터 Text -> Reader
-			Reader isr = new InputStreamReader(is, "UTF-8");
-			BufferedReader br = new BufferedReader(isr);
-			
-			//	클라이언트로 Echo Back을 하기 위한 스트림 열기
-			OutputStream os = socket.getOutputStream();
-			Writer isw = new OutputStreamWriter(os, "UTF-8");
-			BufferedWriter bw = new BufferedWriter(isw);
-			
-			//	데이터 읽기
-			String message;
-			
+			//	클라이언트 연결 대기
 			while(true) {
-				message = br.readLine();	//	한 라인 읽기
-				
-				if (message == null) {	//	더 읽을 데이터 없음
-					System.out.println("[접속 종료!]");
-					break;
-				}
-				
-				System.out.println("클라이언트로부터의 메시지:" + message);
-				System.out.println("Echo Back:" + message);
-				
-				//	클라이언트로 Echo Back
-				bw.write("[Echo]:" + message);
-				bw.newLine();	//	전송
-				bw.flush();	//	버퍼 비우기
+				Socket socket = serverSocket.accept();	//	연결 대기
+				Thread thread = new ServerThread(socket);
+				thread.start();
 			}
 			
-			//	스트림 닫기
-			bw.close();
-			br.close();
 			
 			//	후처리
-			System.out.println("===========");
-			System.out.println("<서버 종료>");
+//			System.out.println("===========");
+//			System.out.println("<서버 종료>");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
